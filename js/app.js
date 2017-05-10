c107c1a72027741d876c7d562c19b55a4491d093
Geraldine Chauca <gchaucae@gmail.com>
2017-05-10 14:00:14 -0500

    Modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 /*
- * Archivo principal de JS
- */
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        ("header").removeClass("large").addClass("small");
+    } else{
+        ("header").removeClass("small").addClass("large");
+    }
+}
+*/
+
+var modal = document.getElementById('myModal');
+
+var img = document.getElementsByClassName('img-items');
+var modalImg = document.getElementById("img01");
+for (var i = 0; i < img.length; i++) {
+	img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+	}
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+    modal.style.display = "none";}
